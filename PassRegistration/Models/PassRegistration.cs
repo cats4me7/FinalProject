e001a8c45f448d5f6a28b84f990489dae94d7723
cats4me7 <cats4me77@yahoo.com>
2017-12-07 11:22:59 -0500

    Added Login enforcement, and finished changing the pages for new passes and editing Passes.

--- PassRegistration/Models/PassRegistration.cs
+++ PassRegistration/Models/PassRegistration.cs
@@ -3,24 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
 namespace PassRegistration.Models
 {
     public class PassRegistration
     {
+        
         public int Id { get; set; }
+        
         public string UserName { get; set; }
+       
         public string Name { get; set; }
         public int NumberInHouse { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public int ZipCode { get; set; }
+        
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string Status { get; set; }
 
         public PassRegistration()
         {
-            Status = "Pending";
+            UserName = "Not Provided.";
+            Name = "Not Provided.";
+            NumberInHouse = 0;
+            Address = "Not Provided.";
+            City = "Not Provided.";
+            ZipCode = 00000;
+            PhoneNumber = "Not Provided.";
+            Email = "Not Provided.";
+            Status = "Pending.";
         }
     }
 
